474eaaa5fc5b647ab9ea07f6a3d80e0848cc632f
Younghee <relief8461@naver.com>
2020-11-29 14:56:42 +0900

    게시판 유저 사진과 아이디는 나옴 제목과 내용 안나옴

--- app/src/main/java/com/example/dietapp/Board/BoardAdapter.java
+++ app/src/main/java/com/example/dietapp/Board/BoardAdapter.java
@@ -11,6 +11,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dietapp.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
--- app/src/main/java/com/example/dietapp/Board/BoardMain.java
+++ app/src/main/java/com/example/dietapp/Board/BoardMain.java
@@ -38,43 +38,34 @@ public class BoardMain extends AppCompatActivity {
 
       recyclerView = findViewById(R.id.b_recyclerview);
       recyclerView.setHasFixedSize(true);
-      adapter = new BoardAdapter(this, boards);
+      /*adapter = new BoardAdapter(this, boards);
       RecyclerView.LayoutManager manager = new LinearLayoutManager(getApplicationContext());
       recyclerView.setLayoutManager(manager);
-      recyclerView.setAdapter(adapter);
+      recyclerView.setAdapter(adapter);*/
 
-      //layoutManager = new LinearLayoutManager(getApplicationContext());
-     // recyclerView.setLayoutManager(layoutManager);
+      layoutManager = new LinearLayoutManager(getApplicationContext());
+      recyclerView.setLayoutManager(layoutManager);
 
       database = FirebaseDatabase.getInstance();
       reference = database.getReference("board");
-      reference.child("board").addChildEventListener(new ChildEventListener() {
+      reference.addListenerForSingleValueEvent(new ValueEventListener() {
          @Override
-         public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-            Board board = snapshot.getValue(Board.class);
-            boards.add(board);
-         }
-
-         @Override
-         public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-         }
-
-         @Override
-         public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-         }
-
-         @Override
-         public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
+         public void onDataChange(@NonNull DataSnapshot snapshot) {
+            boards.clear();
+            for(DataSnapshot snapshot1 : snapshot.getChildren()){
+               Board board = snapshot1.getValue(Board.class);
+               boards.add(board);
+            }
+            adapter.notifyDataSetChanged();
          }
 
          @Override
          public void onCancelled(@NonNull DatabaseError error) {
-
+            Log.e("BoardMainn",String.valueOf(error.toException()));
          }
       });
 
+      adapter = new BoardAdapter(getApplicationContext(),boards);
+      recyclerView.setAdapter(adapter);
    }
 }
